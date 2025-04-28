e9b5394f4288384cc7700e937e02d6cd6c189de4
mhazen <mhazen23@gmail.com>
2025-04-28 19:09:56 -0400

    allows any book to be entered

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 def get_book_text(book):
     with open(book) as f:
         book_contents = f.read()
@@ -6,12 +7,15 @@ from stats import word_count
 from stats import character_count
 from stats import sort_characters
 def main ():
-    book_contents = get_book_text("./books/frankenstein.txt")
+    if len(sys.argv)!=2: #checks number of arguments
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1) #exit with status code 1
+    book_contents = get_book_text(sys.argv[1])
     count = word_count(book_contents)
     char_count = character_count(book_contents)
     char_list = sort_characters(book_contents)
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print("Analyzing book found at " + sys.argv[1])
     print("----------- Word Count ----------")
     print ("Found " + str(count) + " total words")
     print("--------- Character Count -------")
@@ -19,7 +23,6 @@ def main ():
         print(f"{item['char']}: {item['num']}")
     print("============= END ===============")
 main()
-
 
 
         
